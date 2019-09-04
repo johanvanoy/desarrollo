6adc52c061176ec3c33e51ee9796f8ee8ceaa259
Johan Vanoy Suarez <johanvanoys@gmail.com>
2019-09-03 20:43:20 -0500

    comit

---
+++ resources/views/template/template.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="css/menu.css">
+    <title></title>
+  </head>
+
+  <body>
+
+@include("template.menu.menu")
+@yield("contenido")
+
+
+
+
+
+
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="./js/menu.js"></script>
+
+  </body>
+
+</html>
